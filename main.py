12c096e0b9f3dd8581f690de6d089ef2121f8087
AnhraMainyu-dev <maximheilelov@gmail.com>
2026-01-22 10:59:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import ptbot
-import os
 from pytimeparse import parse
-from dotenv import load_dotenv
-load_dotenv()
+from decouple import config
 
-TG_TOKEN = os.environ['TELEGRAM_API']
-BOT = ptbot.Bot(TG_TOKEN)
+TG_TOKEN = config('TELEGRAM_API')
 
 
 def render_progressbar(
@@ -25,36 +22,38 @@ def render_progressbar(
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
 
-def notify_progress(secs_left, chat_id, message_id, progress):
-    BOT.update_message(
+def notify_progress(secs_left, chat_id, message_id, progress, bot):
+    bot.update_message(
         chat_id,
         message_id,
         "У вас осталось {} cекунд.\n{}".format(
             secs_left,
             render_progressbar(progress, progress-secs_left)))
     if secs_left == 0:
-        BOT.send_message(chat_id, "Время вышло!")
+        bot.send_message(chat_id, "Время вышло!")
 
 
-def answer(chat_id, secs):
+def answer(chat_id, secs, bot):
     progress = parse(secs)
     progressbar = render_progressbar(progress, 0)
-    message_id = BOT.send_message(
+    message_id = bot.send_message(
         chat_id,
         "Запускаю таймер:\n {}.".format(progressbar)
     )
-    BOT.create_countdown(
+    bot.create_countdown(
         parse(secs),
         notify_progress,
         chat_id=chat_id,
         message_id=message_id,
-        progress=progress
+        progress=progress,
+        bot = bot
     )
 
 
 def main():
-    BOT.reply_on_message(answer)
-    BOT.run_bot()
+    bot = ptbot.Bot(TG_TOKEN)
+    bot.reply_on_message(answer, bot=bot)
+    bot.run_bot()
 
 
 if __name__ == '__main__':
